d117368b5154f487c4d28280e8747462dcb07512
jub5@sina.com <jub5@sina.com>
2017-10-25 17:57:56 +0800

    command interpreter

---
+++ src/bbkj/org/cn/behaviour/command/Client.java
@@ -0,0 +1,11 @@
+package bbkj.org.cn.behaviour.command;
+
+/**
+ * 信息发送者、传递者、接受者
+ * 从而达到发送者和接受者之间解耦
+ * @author DELL
+ *
+ */
+public class Client {
+
+}
---
+++ src/bbkj/org/cn/behaviour/interpreter/Client.java
@@ -0,0 +1,10 @@
+package bbkj.org.cn.behaviour.interpreter;
+
+/**
+ *	在重复频率比较高，且业务规则频繁变化，但又不是过于繁琐，容易抽象的情况下。
+ *	可构建一个解释器来，对内容进行解释。
+ *	【爬虫】
+ */
+public class Client {
+
+}
